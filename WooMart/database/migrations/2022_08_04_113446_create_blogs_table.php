cfb50010734372f27b0a93d19a4f1ad664ed9f34
ZubaerShimul <zubaer.cse12@gmail.com>
2022-08-04 17:57:06 +0600

    blog table migrate

--- WooMart/database/migrations/2022_08_04_113446_create_blogs_table.php
+++ WooMart/database/migrations/2022_08_04_113446_create_blogs_table.php
@@ -13,15 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('blogs', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('image')->nullable();
-            $table->boolean('is_admin')->default(0);
-            $table->rememberToken();
+            $table->foreignId('author_id')->constrained('users')->onDelete('cascade');
+            $table->string('title');
+            $table->longText('first_section_description');
+            $table->string('quatation')->nullable();
+            $table->longText('second_section_description');
+            $table->string('image');
             $table->timestamps();
         });
     }
@@ -33,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('blogs');
     }
 };
